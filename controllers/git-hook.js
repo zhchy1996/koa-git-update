72d83312c20d152c6704a5fd2f80f664060f0528
chengyuzhang <chengyuzhang@yangqianguan.com>
2020-09-12 18:05:35 +0800

    update sh

--- controllers/git-hook.js
+++ controllers/git-hook.js
@@ -2,7 +2,7 @@
  * @Author: chengyuzhang 
  * @Date: 2020-09-07 19:37:12 
  * @Last Modified by: chengyuzhang
- * @Last Modified time: 2020-09-12 17:37:09
+ * @Last Modified time: 2020-09-12 18:05:06
  */
 
 const exec = require('child_process').execSync
@@ -11,13 +11,13 @@ class UserController {
     // 用户登录
     async update(ctx, next) {
         // do something
-        exec('bash ./sh/build.sh',(...rest) => {
-            console.log(rest);
-        });
-
         ctx.body = {
             status: 'OK'
         }
+
+        exec('bash ./sh/build.sh',(...rest) => {
+            console.log(rest);
+        });
     }
     
     // 用户信息
